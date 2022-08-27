abbbb98b84cd7dc87e1ad8705d056b7c018f0e14
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-26 23:42:18 -0400

    seed

--- back_end/db/seed.sql
+++ back_end/db/seed.sql
@@ -2,5 +2,5 @@
 
 INSERT INTO krystals (name, healing, url, category) VALUES
 ('Black Tourmaline', 'Helps emotional stability', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD9AjZRqxl_sHsJJe0dcyferLaOFyT8SJMXWCKsWuPee8y6nMj9tBPPaHOGo9VNEvIRhg&usqp=CAU', 'anxiety/protection/balance'),
-('Lepidolite', 'Helps sooth distrust, depression and stress', 'https://m.media-amazon.com/images/I/81Ze8ZkJ2kL._AC_SL1500_.jpg', 'anxiety/study/adaptation and change'),
+('Lepidolite', 'Helps sooth distrust, depression and stress', 'https://m.media-amazon.com/images/I/81Ze8ZkJ2kL._AC_SL1500_.jpg', 'anxiety/study/adaptation/change'),
 ('Green Adventurine', 'Increases leadership and decisioon making', 'https://m.media-amazon.com/images/I/81uGSz8zdOL._AC_SL1500_.jpg', 'motivation/prosperity/creativity');
