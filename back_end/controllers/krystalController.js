6b667f2d10f4677fbd2053379d0f4488f4d31cbd
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-27 13:12:14 -0400

    create and show

--- back_end/controllers/krystalController.js
+++ back_end/controllers/krystalController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const krystals = express.Router();
-const { getAllKrystals } = require("../queries/krystals");
+const { getAllKrystals, getKrystal, createKrystal } = require("../queries/krystals");
 
 // // INDEX
 // krystals.get("/", async (req, res) => {});
@@ -11,13 +11,35 @@ const { getAllKrystals } = require("../queries/krystals");
 //   res.json({ status: "ok" });
 // });
 
-
+// INDEX
 krystals.get("/", async (req, res) => {
     const allKrystals = await getAllKrystals();
     if (allKrystals[0]) {
       res.status(200).json(allKrystals);
     } else {
       res.status(500).json({ error: "server error" });
+    }
+  });
+
+  // SHOW
+krystals.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const krystal = await getKrystal(id);
+    if (krystal) {
+      res.json(krystal);
+    } else {
+      res.status(404).json({ error: "not found" });
+    }
+  });
+
+
+// CREATE
+krystals.post("/", async (req, res) => {
+    try {
+      const krystal = await createKrystal(req.body);
+      res.json(krystal);
+    } catch (error) {
+      res.status(400).json({ error: error });
     }
   });
 
--- back_end/queries/krystals.js
+++ back_end/queries/krystals.js
@@ -1,5 +1,6 @@
 const db = require("../db/dbConfig.js");
 
+// INDEX
 const getAllKrystals = async () => {
     try {
         const allKrystals = await db.any("SELECT * FROM krystals");
@@ -9,5 +10,27 @@ const getAllKrystals = async () => {
       }
     };
 
+    // SHOW
+    const getKrystal = async (id) => {
+        try {
+          const oneKrystal = await db.one("SELECT * FROM krystals WHERE id=$1", id);
+          return oneKrystal;
+        } catch (error) {
+          return error;
+        }
+      };
 
-module.exports = { getAllKrystals };
+      // CREATE
+const createKrystal = async (krystal) => {
+    try {
+      const newKrystal = await db.one(
+        "INSERT INTO krystals (name, healing, url, category) VALUES($1, $2, $3, $4) RETURNING *",
+        [krystal.name, krystal.healing, krystal.url, krystal.category, ]
+      );
+      return newKrystal;
+    } catch (error) {
+      return error;
+    }
+  };
+
+module.exports = { getAllKrystals, getKrystal, createKrystal };
